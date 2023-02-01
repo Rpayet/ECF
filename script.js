606121496aeaf09dc863fd6f6e0c0a898f1c70c7
Robin Payet <robinpayet@gmail.com>
2023-02-01 16:03:39 +0100

    Laptop - JS Purchase Button Setup

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Toggle NavBar Menu on Mobile
+// Toggle NavBar Menu on Mobile
 
 let navDisplay = document.querySelector('#navDisplay');
 let navClose = document.querySelector('#navClose');
@@ -13,4 +13,23 @@ navClose.addEventListener('click', () => {
     navBar.classList.toggle('hide');
     navBar.classList.toggle('show');
 });
+
+// Valider la commande 
+
+let purchase = document.querySelector('#purchase');
+let purchaseBtn = document.querySelector('#purchase-btn');
+
+purchaseBtn.addEventListener('click', () => {
+
+    let div = document.createElement('div');
+    div.classList.add('purchase-valid');
+    let paragraph = document.createElement('p');
+    div.appendChild(paragraph);
+    paragraph.innerHTML = "Votre commande a été prise en compte."
+    purchase.innerHTML= '';
+    purchase.appendChild(div)
+
+})
+
+
 
