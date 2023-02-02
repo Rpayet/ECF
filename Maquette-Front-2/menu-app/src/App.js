cfd4de2089925857bca163806826da1aaa390fcb
Rpayet <robinpayet@gmail.com>
2023-02-02 12:13:39 +0100

    Desktop - REACT HTML Setup WIP

--- Maquette-Front-2/menu-app/src/App.js
+++ Maquette-Front-2/menu-app/src/App.js
@@ -1,10 +1,42 @@
+import star from './img/star.svg';
+
 
 function App() {
   return (
     <div className="container">
+
       <header className="App-header">
-        Nothing to see here.
+
+        <h1>Découvrez notre <span className="underline">menu.</span></h1>
+
+        <ul className='navBar'>
+          <li className="button"><p>Tout</p></li>
+          <li className="button"><p>Viandes</p></li>
+          <li className="button"><p>Légumes</p></li>
+        </ul>
+
       </header>
+
+      <section className="card-ctr">
+
+        <div className="card">
+
+          <div className="top">
+            <img src="" alt="meal" />
+            <div className="rate">
+              <img src={star} alt="star"/>
+              <p>5 <span> (87) </span></p>
+            </div>
+          </div>
+
+          <div classname="bottom">
+            <h3>Mélange de légume</h3>
+            <p>Salade de tomates et carottes</p>
+            <h3>5.99€</h3>
+          </div>
+        </div>
+
+      </section>
     </div>
   );
 }
