02c1de12874b458e3caa35f0c05b9aaa31902212
Rpayet <robinpayet@gmail.com>
2023-02-02 14:06:16 +0100

    Desktop - REACT CSS Card Setup

--- Maquette-Front-2/menu-app/src/App.js
+++ Maquette-Front-2/menu-app/src/App.js
@@ -22,6 +22,23 @@ function App() {
         <div className="card">
 
           <div className="top">
+            <img className="meal-img" src="" alt="meal" />
+            <div className="rate">
+              <img src={star} alt="star"/>
+              <p>5 <span> (87) </span></p>
+            </div>
+          </div>
+
+          <div className="bottom">
+            <h3>Mélange de légume</h3>
+            <p>Salade de tomates et carottes</p>
+            <h3>5.99€</h3>
+          </div>
+        </div>
+
+        <div className="card">
+
+          <div className="top">
             <img src="" alt="meal" />
             <div className="rate">
               <img src={star} alt="star"/>
@@ -29,12 +46,51 @@ function App() {
             </div>
           </div>
 
-          <div classname="bottom">
+          <div className="bottom">
             <h3>Mélange de légume</h3>
             <p>Salade de tomates et carottes</p>
             <h3>5.99€</h3>
           </div>
+
         </div>
+
+        <div className="card">
+
+          <div className="top">
+            <img src="" alt="meal" />
+            <div className="rate">
+              <img src={star} alt="star"/>
+              <p>5 <span> (87) </span></p>
+            </div>
+          </div>
+
+          <div className="bottom">
+            <h3>Mélange de légume</h3>
+            <p>Salade de tomates et carottes</p>
+            <h3>5.99€</h3>
+          </div>
+          
+        </div>
+
+        <div className="card">
+
+          <div className="top">
+            <img src="" alt="meal" />
+            <div className="rate">
+              <img src={star} alt="star"/>
+              <p>5 <span> (87) </span></p>
+            </div>
+          </div>
+
+          <div className="bottom">
+            <h3>Mélange de légume</h3>
+            <p>Salade de tomates et carottes</p>
+            <h3>5.99€</h3>
+          </div>
+          
+        </div>
+
+        
 
       </section>
     </div>
